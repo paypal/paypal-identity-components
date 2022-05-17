6e84c817065b26b1681910d89891d397ea249746
adolfo flores <adoflores@paypal.com>
2022-05-17 16:02:40 -0500

    Change auth button iframe payload from ice to smart components

--- src/ui/button/button.jsx
+++ src/ui/button/button.jsx
@@ -41,29 +41,9 @@ function PPSymbol({ style } : LogoProps) : ComponentNode<LogoProps> {
 
 }
 
-type LabelProps = {|
-    style : ButtonStyle,
-    content : string
-|};
-
-function Label({ style, content } : LabelProps) : ?ComponentNode<{||}> {
-    const { label } = style;
-    const text  = content;
-    if (!label) {
-        return <Text>Log in with PayPal</Text>;
-    }
-
-    if (label) {
-        return <Text>{ label }</Text>;
-    }
-
-    throw new Error(`Unsupported button label: ${ label || text }`);
-}
-
 export function AuthButton(props : ButtonProps) : ElementNode {
     const { onClick = noop } = props;
-    const { fundingSource, style,  env,
-        nonce, customLabel } = normalizeButtonProps(props);
+    const { fundingSource, style,  env, nonce } = normalizeButtonProps(props);
     const { shape, color } = style;
     const clickHandler = (event, opts) => {
         event.preventDefault();
@@ -78,7 +58,6 @@ export function AuthButton(props : ButtonProps) : ElementNode {
             clickHandler(event, opts);
         }
     };
-
     return (
         <div class={ [
             CLASS.CONTAINER,
@@ -89,30 +68,28 @@ export function AuthButton(props : ButtonProps) : ElementNode {
             <Style
                 nonce={ nonce }
                 style={ style }
-            />
+        />
 
             <div
                 role='button'
                 { ...{
-                    [ATTRIBUTE.BUTTON]:         true,
-                    [ATTRIBUTE.FUNDING_SOURCE]: fundingSource
-                } }
+            [ATTRIBUTE.BUTTON]:         true,
+            [ATTRIBUTE.FUNDING_SOURCE]: fundingSource
+        } }
                 class={ [
-                    CLASS.BUTTON,
-                    `${ CLASS.SHAPE }-${ shape }`,
-                    `${ CLASS.ENV }-${ env }`,
-                    `${ CLASS.COLOR }-${ color }`,
-                    `${ CLASS.TEXT_COLOR }-${ TEXT_COLOR.WHITE }`
-                ].join(' ') }
+            CLASS.BUTTON,
+            `${ CLASS.SHAPE }-${ shape }`,
+            `${ CLASS.ENV }-${ env }`,
+            `${ CLASS.COLOR }-${ color }`,
+            `${ CLASS.TEXT_COLOR }-${ TEXT_COLOR.WHITE }`
+        ].join(' ') }
                 onClick={ clickHandler }
                 onKeyPress={ keypressHandler }
                 tabindex='0'>
-
-                { customLabel &&
-                    <div class={ CLASS.BUTTON_LABEL }>
-                        <PPSymbol style={ style } />
-                        <Label style={ style } content={ customLabel } />
-                    </div>}
+                <div class={ CLASS.BUTTON_LABEL }>
+                    <PPSymbol style={ style } />
+                    <Text>{ style.label || 'Log in with PayPal' }</Text>
+                </div>
             </div>
         </div>
     );
