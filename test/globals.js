bc0b5de7962c1f41512f29c64ce6d7bd40d6cfc3
Westin Wrzesinski <westeezy@users.noreply.github.com>
2023-07-15 16:16:50 -0500

    ci: use sandbox urls (#41)

--- test/globals.js
+++ test/globals.js
@@ -13,7 +13,7 @@ export const testGlobals = {
 
   __PROTOCOL__: "http",
   __PORT__: 8000,
-  __STAGE_HOST__: "msmaster.qa.paypal.com",
+  __STAGE_HOST__: "sandbox.paypal.com",
   __HOST__: "test.paypal.com",
   __HOSTNAME__: "test.paypal.com",
   __SDK_HOST__: "test.paypal.com",
@@ -23,5 +23,5 @@ export const testGlobals = {
   __COMPONENTS__: ["button"],
   __CORRELATION_ID__: "abc123",
   __PAYPAL_DOMAIN__: "mock://www.paypal.com",
-  __PAYPAL_API_DOMAIN__: "mock://msmaster.qa.paypal.com",
+  __PAYPAL_API_DOMAIN__: "mock://sandbox.paypal.com",
 };
